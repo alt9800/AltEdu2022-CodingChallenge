957ad54e8a19c52b658aa7d0ca3d098596fbfde9
alt9800 <51439199+alt9800@users.noreply.github.com>
2022-02-01 23:33:22 +0900

    Create main.js

---
+++ day1/main.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  noFill();
+  stroke(0, 0, 0);
+  rect(width / 2 -120, width / 2 -90 , 240,180);
+  
+  stroke(255, 0, 0);
+  fill(255,0,0);  
+  circle(width / 2, height / 2, 90);
+}
